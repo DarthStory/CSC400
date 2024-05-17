0e7a6665c678440e11f9722aaaa82cf177909a36
David Owen <david.owen@csuglobal.edu>
2024-05-17 07:58:45 -0500

    working on CT

--- ADTBag/src/bag/Bag.java
+++ ADTBag/src/bag/Bag.java
@@ -1,5 +1,32 @@
 package bag;
 
-public class Bag {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+class Bag<object> implements Iterable<object> {
+    private ArrayList<object> items = new ArrayList<>();
+
+    public void add(object item) {
+        items.add(item);
+    }
+
+    public void remove(object item) {
+    	items.remove(item);
+    }
+    public boolean contanins() {
+        return items.contains(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+    
+    public void clear() {
+    	items.clear();
+    }
+
+    @Override
+    public Iterator<object> iterator() {
+        return items.iterator();
+    }
 }
---
+++ ADTBag/src/bag/BagMain.java
@@ -0,0 +1,45 @@
+package bag;
+
+import java.util.HashMap;
+
+public class BagMain {
+
+	public static void main(String[] args) {
+		
+		Bag<Object> stuff = new Bag<>();
+		
+		stuff.add("Name");
+		stuff.add(32.54);
+		stuff.add("Hello");
+		stuff.add(23);
+		stuff.add("Name");
+		stuff.add(1999.9999);
+		stuff.add("Name");
+		stuff.add(32.54);
+		stuff.add("Hello");
+		stuff.add(23);
+		stuff.add("Name");
+		stuff.add(1999.9999);
+		
+		// Counting word frequencies
+        HashMap<Object, Integer> countStuff = new HashMap<>();
+        for (Object thing : stuff) {
+            countStuff.put(thing, countStuff.getOrDefault(thing, 0) + 1);
+        }
+
+        // Printing word counts
+        System.out.println("Stuff in Bag");
+        countStuff.forEach((key, value) -> System.out.println(key + ": " + value));
+		
+		stuff.clear();
+//		stuff.remove("Name");
+
+        for (Object thing : stuff) {
+            countStuff.put(thing, countStuff.getOrDefault(thing, 0) + 1);
+        }
+
+        System.out.println("\nModified Stuff in Bag");
+        countStuff.forEach((key, value) -> System.out.println(key + ": " + value));
+        
+	}
+}
