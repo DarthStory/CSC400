1d5a1565d2c3ea711f0341c3c31e6082971d6e34
David Owen <david.owen@csuglobal.edu>
2024-06-08 12:08:20 -0500

    catchup 2

--- PostFix/src/postFix/MainPostFixCalc.java
+++ PostFix/src/postFix/MainPostFixCalc.java
@@ -6,7 +6,8 @@ public class MainPostFixCalc {
 		
 		PostfixCalculator calculator = new PostfixCalculator();
 		
-			
+		String input1 = "34+5*";
+		System.out.println("The answer of " + input1 + " is: " + calculator.evaluatePostfix(input1));
 	}
 
 }
--- PostFix/src/postFix/PostfixCalculator.java
+++ PostFix/src/postFix/PostfixCalculator.java
@@ -1,7 +1,46 @@
 package postFix;
 
+import java.util.Stack;
+
 public class PostfixCalculator {
 
-	// stuff here
+	// Method to evaluate the expression
+	public int evaluatePostfix(String expression) {
+		// first Implement the stack
+		Stack<Integer> stack = new Stack<>();
+		
+		// create a for loop to go through the expression
+		for(int i = 0; i < expression.length(); i++) {
+			char c = expression.charAt(i);
+			
+			// if character is a number, push it to the stack
+			if(Character.isDigit(c)) {
+				stack.push(c - '0');
+			}else {
+				// if an operator, then pop elements and apply operator
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				
+				switch(c) {
+				case '+':
+					stack.push(num1 + num2);
+					break;
+				case '-':
+					stack.push(num1 - num2);
+					break;
+				case '*':
+					stack.push(num1 * num2);
+					break;
+				case '/':
+					stack.push(num1 / num2);
+					break;
+				case '%':
+					stack.push(num1 % num2);
+					break;
+				}
+			}
+		}
+		return stack.pop();
+	}
 	
 }
