deca2ca92ec548c7af20d0ac63eb423923aa6731
David Owen <david.owen@csuglobal.edu>
2024-06-09 11:08:17 -0500

    try/catch for mainss

--- PostFix/src/postFix/MainPostFixCalc.java
+++ PostFix/src/postFix/MainPostFixCalc.java
@@ -5,35 +5,43 @@ import java.util.EmptyStackException;
 public class MainPostFixCalc {
 
 	public static void main(String[] args) {
-		
+		// creates the PostfixCalculator object
 		PostfixCalculator calculator = new PostfixCalculator();
 		
+		// Adds strings and tries to evaluate the string to run the calculation
+		// If it fails, it lets you know. 
 		try {
-		String input1 = "34+5*";
+		String input1 = "23*";
 		System.out.println("The answer of " + input1 + " is: " + calculator.evaluatePostfix(input1));
 		}catch (EmptyStackException e){
-			System.out.println("This is not proper Postfix notation");
+			System.out.println("This is not proper Postfix notation" + e.getMessage());
 		}
 		try {
 		String input2 = "53+7*";
 		System.out.println("The answer of " + input2 + " is: " + calculator.evaluatePostfix(input2));
 		}catch (EmptyStackException e) {
-			System.out.println("This is not proper Postfix notation");
+			System.out.println("This is not proper Postfix notation" + e.getMessage());
 		}
 		
 		try {
 		String input3 = "42*+";
 		System.out.println("The answer of " + input3 + " is: " + calculator.evaluatePostfix(input3));
 		}catch (EmptyStackException e) {
-			System.out.println("This is not proper Postfix notation");
+			System.out.println("This is not proper Postfix notation" + e.getMessage());
 		}
 		
 		try {
-		String input4 = "34+6*";
+		String input4 = "324+6*";
 		System.out.println("The answer of " + input4 + " is: " + calculator.evaluatePostfix(input4));
 		}catch (EmptyStackException e) {
-			System.out.println("This is not proper Postfix notation");
+			System.out.println("This is not proper Postfix notation" + e.getMessage());
 		}
+		try {
+			String input5 = "23+";
+			System.out.println("The answer of " + input5 + " is: " + calculator.evaluatePostfix(input5));
+			}catch (EmptyStackException e) {
+				System.out.println("This is not proper Postfix notation" + e.getMessage());
+			}
 	}
 
 }
--- PostFix/src/postFix/PostfixCalculator.java
+++ PostFix/src/postFix/PostfixCalculator.java
@@ -14,33 +14,48 @@ public class PostfixCalculator {
 			char c = expression.charAt(i);
 			
 			// if character is a number, push it to the stack
-			if(Character.isDigit(c)) {
-				stack.push(c - '0');
-			}else {
+				if(Character.isDigit(c)) {
+					stack.push(c - '0');
+				}else {
 				// if an operator, then pop elements and apply operator
 				int num1 = stack.pop();
 				int num2 = stack.pop();
 				
+				System.out.println("Popped from stack: " + num1 + ", " + num2); // debug
+				
 				switch(c) {
 				case '+':
 					stack.push(num1 + num2);
+					System.out.println("Performed addition: " + num1 + " + " + num2); // Debug print
 					break;
 				case '-':
 					stack.push(num1 - num2);
+					System.out.println("Performed subtraction: " + num1 + " - " + num2); // Debug print
 					break;
 				case '*':
 					stack.push(num1 * num2);
+					System.out.println("Performed multiplication: " + num1 + " * " + num2); // Debug print
 					break;
 				case '/':
+					if(num2 == 0) {
+						throw new ArithmeticException("Arithmetic Error");
+					}
 					stack.push(num1 / num2);
+					System.out.println("Performed division: " + num1 + " / " + num2); // Debug print
 					break;
 				case '%':
+					if(num2 == 0) {
+						throw new ArithmeticException("Arithmetic Error");
+					}
 					stack.push(num1 % num2);
-					break;
+					System.out.println("Performed modulus: " + num1 + " % " + num2); // Debug print
+                    break;
 				}
-			}
+		}
+		if(stack.size() != 1) {
+			throw new IllegalArgumentException("Error: Invalid postfix expression, stack size is " + stack.size());
 		}
 		return stack.pop();
+		}
 	}
-	
 }
