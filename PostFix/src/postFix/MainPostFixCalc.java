69ee239e594c9baad6a0d9b89298e9a6cdd1e8c6
David Owen <david.owen@csuglobal.edu>
2024-06-09 09:12:55 -0500

    try/catch for main

--- PostFix/src/postFix/MainPostFixCalc.java
+++ PostFix/src/postFix/MainPostFixCalc.java
@@ -1,13 +1,39 @@
 package postFix;
 
+import java.util.EmptyStackException;
+
 public class MainPostFixCalc {
 
 	public static void main(String[] args) {
 		
 		PostfixCalculator calculator = new PostfixCalculator();
 		
+		try {
 		String input1 = "34+5*";
 		System.out.println("The answer of " + input1 + " is: " + calculator.evaluatePostfix(input1));
+		}catch (EmptyStackException e){
+			System.out.println("This is not proper Postfix notation");
+		}
+		try {
+		String input2 = "53+7*";
+		System.out.println("The answer of " + input2 + " is: " + calculator.evaluatePostfix(input2));
+		}catch (EmptyStackException e) {
+			System.out.println("This is not proper Postfix notation");
+		}
+		
+		try {
+		String input3 = "42*+";
+		System.out.println("The answer of " + input3 + " is: " + calculator.evaluatePostfix(input3));
+		}catch (EmptyStackException e) {
+			System.out.println("This is not proper Postfix notation");
+		}
+		
+		try {
+		String input4 = "34+6*";
+		System.out.println("The answer of " + input4 + " is: " + calculator.evaluatePostfix(input4));
+		}catch (EmptyStackException e) {
+			System.out.println("This is not proper Postfix notation");
+		}
 	}
 
 }
