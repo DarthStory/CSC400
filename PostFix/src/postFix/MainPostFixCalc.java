4ba8d8c5b5467a323153dab9ef1b96673134af78
David Owen <david.owen@csuglobal.edu>
2024-06-09 19:11:57 -0500

    no multi digit processss

--- PostFix/src/postFix/MainPostFixCalc.java
+++ PostFix/src/postFix/MainPostFixCalc.java
@@ -1,47 +1,68 @@
 package postFix;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.util.List;
 import java.util.EmptyStackException;
 
 public class MainPostFixCalc {
 
-	public static void main(String[] args) {
-		// creates the PostfixCalculator object
-		PostfixCalculator calculator = new PostfixCalculator();
-		
-		// Adds strings and tries to evaluate the string to run the calculation
-		// If it fails, it lets you know. 
-		try {
-			String input1 = "23*3*";
-			System.out.println("The answer of " + input1 + " is: " + calculator.evaluatePostfix(input1)+ "\n");
-		}catch (EmptyStackException e){
-			System.out.println("This is not proper Postfix notation" + e.getMessage()+ "\n");
-		}
-		try {
-			String input2 = "53+7*";
-			System.out.println("The answer of " + input2 + " is: " + calculator.evaluatePostfix(input2)+ "\n");
-		}catch (EmptyStackException e) {
-			System.out.println("This is not proper Postfix notation" + e.getMessage()+ "\n");
-		}
-		
-		try {
-			String input3 = "42*+";
-			System.out.println("The answer of " + input3 + " is: " + calculator.evaluatePostfix(input3)+ "\n");
-		}catch (EmptyStackException e) {
-			System.out.println("This is not proper Postfix notation" + e.getMessage()+ "\n");
-		}
-		
-		try {
-			String input4 = "34+6*";
-			System.out.println("The answer of " + input4 + " is: " + calculator.evaluatePostfix(input4)+ "\n");
-		}catch (EmptyStackException e) {
-			System.out.println("This is not proper Postfix notation" + e.getMessage()+ "\n");
-		}
-		try {
-			String input5 = "23+";
-			System.out.println("The answer of " + input5 + " is: " + calculator.evaluatePostfix(input5)+ "\n");
-			}catch (EmptyStackException e) {
-				System.out.println("This is not proper Postfix notation" + e.getMessage()+ "\n");
-			}
-	}
+    public static void main(String[] args) {
+        // creates the PostfixCalculator object
+        PostfixCalculator calculator = new PostfixCalculator();
 
-}
+        // Adds strings and tries to evaluate the string to run the calculation
+        // If it fails, it lets you know.
+        try {
+            String input1 = "23*3*";
+            System.out.println("The answer of " + input1 + " is: " + calculator.evaluatePostfix(input1) + "\n");
+        } catch (EmptyStackException e) {
+            System.out.println("This is not proper Postfix notation" + e.getMessage() + "\n");
+        }
+        try {
+            String input2 = "53+7";
+            System.out.println("The answer of " + input2 + " is: " + calculator.evaluatePostfix(input2) + "\n");
+        } catch (EmptyStackException e) {
+            System.out.println("This is not proper Postfix notation" + e.getMessage() + "\n");
+        }
+
+        try {
+            String input3 = "42*+";
+            System.out.println("The answer of " + input3 + " is: " + calculator.evaluatePostfix(input3) + "\n");
+        } catch (EmptyStackException e) {
+            System.out.println("This is not proper Postfix notation" + e.getMessage() + "\n");
+        }
+
+        try {
+            String input4 = "34+6*";
+            System.out.println("The answer of " + input4 + " is: " + calculator.evaluatePostfix(input4) + "\n");
+        } catch (EmptyStackException e) {
+            System.out.println("This is not proper Postfix notation" + e.getMessage() + "\n");
+        }
+        try {
+            String input5 = "23+";
+            System.out.println("The answer of " + input5 + " is: " + calculator.evaluatePostfix(input5) + "\n");
+        } catch (EmptyStackException e) {
+            System.out.println("This is not proper Postfix notation" + e.getMessage() + "\n");
+        }
+        FileReadExample(calculator);
+    }
+
+//    private static void FileReadExample() {
+//    }
+
+    private static void FileReadExample(PostfixCalculator calculator) {
+            Path filePath = Paths.get("C:\\Temp\\Postfix.txt");
+            try {
+                List<String> lines = Files.readAllLines(filePath);
+                for (String line : lines) {
+                   //System.out.println(line);
+                    System.out.println("The answer of " + line + " is: " + calculator.evaluatePostfix(line) + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
--- PostFix/src/postFix/PostfixCalculator.java
+++ PostFix/src/postFix/PostfixCalculator.java
@@ -4,55 +4,55 @@ import java.util.Stack;
 
 public class PostfixCalculator {
 
-	// Method to evaluate the expression
-	public int evaluatePostfix(String expression) {
-		// first Implement the stack
-		Stack<Integer> stack = new Stack<>();
-		
-		// create a for loop to go through the expression
-		for(int i = 0; i < expression.length(); i++) {
-			char c = expression.charAt(i);
-			
-			// if character is a number, push it to the stack
-				if(Character.isDigit(c)) {
-					stack.push(c - '0');
-			}else {
-			// if an operator, then pop elements and apply operator
-			int num1 = stack.pop();
-			int num2 = stack.pop();
-				
-			System.out.println("Popped from stack: " + num1 + ", " + num2); // debug
-				
-				switch(c) {
-				case '+':
-					stack.push(num1 + num2);
-					System.out.println("Performed addition: " + num1 + " + " + num2); // Debug print
-					break;
-				case '-':
-					stack.push(num1 - num2);
-					System.out.println("Performed subtraction: " + num1 + " - " + num2); // Debug print
-					break;
-				case '*':
-					stack.push(num1 * num2);
-					System.out.println("Performed multiplication: " + num1 + " * " + num2); // Debug print
-					break;
-				case '/':
-					if(num2 == 0) {
-						throw new ArithmeticException("Arithmetic Error");
-					}
-					stack.push(num1 / num2);
-					System.out.println("Performed division: " + num1 + " / " + num2); // Debug print
-					break;
-				case '%':
-					if(num2 == 0) {
-						throw new ArithmeticException("Arithmetic Error");
-					}
-					stack.push(num1 % num2);
-					System.out.println("Performed modulus: " + num1 + " % " + num2); // Debug print
-	                   break;
-				}
-			}
-		}
-		return stack.pop();
-	}
+    // Method to evaluate the expression
+    public int evaluatePostfix(String expression) {
+        // first Implement the stack
+        Stack<Integer> stack = new Stack<>();
+
+        // create a for loop to go through the expression
+        for(int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            // if character is a number, push it to the stack
+            if(Character.isDigit(c)) {
+                stack.push(c - '0');
+            }else {
+                // if an operator, then pop elements and apply operator
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                System.out.println("Popped from stack: " + num1 + ", " + num2); // debug
+
+                switch(c) {
+                    case '+':
+                        stack.push(num1 + num2);
+                        System.out.println("Performed addition: " + num1 + " + " + num2); // Debug print
+                        break;
+                    case '-':
+                        stack.push(num1 - num2);
+                        System.out.println("Performed subtraction: " + num1 + " - " + num2); // Debug print
+                        break;
+                    case '*':
+                        stack.push(num1 * num2);
+                        System.out.println("Performed multiplication: " + num1 + " * " + num2); // Debug print
+                        break;
+                    case '/':
+                        if(num2 == 0) {
+                            throw new ArithmeticException("Arithmetic Error");
+                        }
+                        stack.push(num1 / num2);
+                        System.out.println("Performed division: " + num1 + " / " + num2); // Debug print
+                        break;
+                    case '%':
+                        if(num2 == 0) {
+                            throw new ArithmeticException("Arithmetic Error");
+                        }
+                        stack.push(num1 % num2);
+                        System.out.println("Performed modulus: " + num1 + " % " + num2); // Debug print
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
 }
