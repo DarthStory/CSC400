4295119b7d7a6db2bf14cec18419e0af50f71764
David Owen <david.owen@csuglobal.edu>
2024-06-22 07:26:20 -0500

    sorting algorithms

--- SortingAlgorithms/src/sortAlgorithm/InsertionSort.java
+++ SortingAlgorithms/src/sortAlgorithm/InsertionSort.java
@@ -2,19 +2,19 @@ package sortAlgorithm;
 
 public class InsertionSort {
 
-	public static void insertionSort(int[] arr2) {
-		int n = arr2.length;
+	public static void insertionSort(int[] arr) {
+		int n = arr.length;
 	    for (int i = 1; i < n; ++i) {
-	        int key = arr2[i];
+	        int key = arr[i];
 	        int j = i - 1;
 	
 	        // Move elements of arr2[0..i-1], that are greater than key,
 	        // to one position ahead of their current position
-	        while (j >= 0 && arr2[j] > key) {
-	            arr2[j + 1] = arr2[j];
+	        while (j >= 0 && arr[j] > key) {
+	            arr[j + 1] = arr[j];
 	            j = j - 1;
 	        }
-	        arr2[j + 1] = key;
+	        arr[j + 1] = key;
 	    }
 	}
 }
--- SortingAlgorithms/src/sortAlgorithm/Main.java
+++ SortingAlgorithms/src/sortAlgorithm/Main.java
@@ -4,61 +4,73 @@ import java.util.Random;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Random rand = new Random();
-		 int[] arr = {12, 11, 13};
-		 int[] arr1 = new int[1000000];
-		 int[] arr2 = {12, 11, 13, 5, 6};
-		 int[] arr3 = new int[1000000];
-		 
-		 long start;
-		 long duration;
-		 
-	     int n = arr.length;
-	     start = System.nanoTime();	        
-	     	MergeSort.mergeSort(arr, 0, n - 1);
-	     duration = (System.nanoTime() - start) / 1000000;
-	     
-	     System.out.println("Sorted array: ");
-	     for(int num : arr) {
-	    	 System.out.print(num + " ");	        	
-	     }	     
-	     System.out.println("\n" + duration + "ms");
-//
-//
-	     int a = arr1.length;
-	     start = System.nanoTime();
-	     	MergeSort.mergeSort(arr1, 0, a - 1);
-	     duration = (System.nanoTime() - start) / 1000000;
-	     
-	     System.out.println("Sorted array: ");
-	     for(int num : arr1) {
-	    	 System.out.print(num + " ");	        	
-	     }	     
-	     System.out.println("\n" + duration + "ms");
-//   
-//   
-	     start = System.nanoTime();
-	     
-	        InsertionSort.insertionSort(arr2);
-	        System.out.println("Sorted array: ");
-	        for (int num : arr2) {
-	            System.out.print(num + " ");
-	        }
-	     duration = (System.nanoTime() - start) / 1000000;
-		 System.out.println("\n" + duration + "ms");		 
-//		 
-//		 
-		 start = System.nanoTime();
-		 InsertionSort.insertionSort(arr3);
-	        System.out.println("Sorted array: ");
-	        for (int num : arr3) {
-	            System.out.print(num + " ");
-	        }	     
-	     duration = (System.nanoTime() - start) / 1000000;
-		 System.out.println("\n" + duration + "ms");
-		 
-		 
-	}
+    public static void main(String[] args) {
+        
+        Random rand = new Random();
+        int[] arr = new int[10];
+        int[] arr1 = new int[10000];
+        int[] arr2 = new int[10];
+        int[] arr3 = new int[10000];
+        
+        // Fill arrays with random numbers
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100); // Random numbers between 0 and 99
+        }
+        
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = rand.nextInt(1000000); // Random numbers between 0 and 999999
+        }
+        
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = rand.nextInt(10); // Random numbers between 0 and 9
+        }
+        
+        for (int i = 0; i < arr3.length; i++) {
+            arr3[i] = rand.nextInt(1000000); // Random numbers between 0 and 999999
+        }
+
+        long start;
+        long duration;
+        
+        int n = arr.length;
+        start = System.nanoTime();        
+        MergeSort.mergeSort(arr, 0, n - 1);
+        duration = (System.nanoTime() - start) / 1000000;
+        
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");            
+        }        
+        System.out.println("\n" + duration + "ms");
+        
+        int a = arr1.length;
+        start = System.nanoTime();
+        MergeSort.mergeSort(arr1, 0, a - 1);
+        duration = (System.nanoTime() - start) / 1000000;
+        
+        System.out.println("Sorted array: ");
+        for (int num : arr1) {
+            System.out.print(num + " ");            
+        }        
+        System.out.println("\n" + duration + "ms");
+        
+        start = System.nanoTime();
+        InsertionSort.insertionSort(arr2);
+        System.out.println("Sorted array: ");
+        for (int num : arr2) {
+            System.out.print(num + " ");
+        }
+        duration = (System.nanoTime() - start) / 1000000;
+        System.out.println("\n" + duration + "ms");        
+         
+        start = System.nanoTime();
+        InsertionSort.insertionSort(arr3);
+        System.out.println("Sorted array: ");
+        for (int num : arr3) {
+            System.out.print(num + " ");
+        }        
+        duration = (System.nanoTime() - start) / 1000000;
+        System.out.println("\n" + duration + "ms");
+        
+    }
 }
