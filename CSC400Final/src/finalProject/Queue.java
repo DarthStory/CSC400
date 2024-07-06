0a645694b60cf2cafa5237e23e17268e9f56416d
David Owen <david.owen@csuglobal.edu>
2024-07-06 09:50:37 -0500

    update3

--- CSC400Final/src/finalProject/Queue.java
+++ CSC400Final/src/finalProject/Queue.java
@@ -21,6 +21,40 @@ public class Queue {
 		}
 	}
 	
+	public static void quickSort(List<Person> list, int left, int right,
+			java.util.Comparator<Person> comparator) {
+        if (left < right) {
+            int pivotIndex = partition(list, left, right, comparator);
+
+            quickSort(list, left, pivotIndex - 1, comparator);
+            quickSort(list, pivotIndex + 1, right, comparator);
+        }
+    }
+
+    public static int partition(List<Person> list, int left, int right,
+    		java.util.Comparator<Person> comparator) {
+        Person pivot = list.get(right);
+        int i = (left - 1);
+
+        for (int j = left; j < right; j++) {
+            if (comparator.compare(list.get(j), pivot) > 0) {
+                i++;
+                Person temp = list.get(i);
+                list.set(i, list.get(j));
+                list.set(j, temp);
+            }
+        }
+
+        Person temp = list.get(i + 1);
+        list.set(i + 1, list.get(right));
+        list.set(right,  temp);
+        return i + 1;
+    }
+    
+    public void lastNameDescending() {
+    	quickSort(queue, 0, queue.size() -1, (p1, p2) -> p2.lastName.compareTo(p1.lastName));
+    }
 	
-	
+    public void 
+    
 }
